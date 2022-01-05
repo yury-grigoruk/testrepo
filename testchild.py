4e0a70a54f80391106737659d334bbbffd559054
yurybaks <96960828+yurybaks@users.noreply.github.com>
2022-01-04 21:35:53 -0500

    Update testchild.py

--- testchild.py
+++ testchild.py
@@ -1,2 +1,44 @@
-#adding new file in child branch
-print ("Inside the child branch")
+
+<H1> My Jupyter Notebood on IBM Watson Studio <H1>
+
+
+
+
+
+ <b> Yury Grigoruk <b>
+    
+
+
+I am building my data science career
+
+*I am interested in data science because I love to play with numbers* 
+
+**Function**
+
+def myltiplication(x,y):
+    return x*y
+print(myltiplication(20,35))
+
+
+* for 
+* good
+* success
+* you
+* should
+* work
+* hard
+
+Markdown | Less | Pretty
+--- | --- | ---
+*Still* | `renders` | **nicely**
+1 | 2 | 3
+
+
+
+
+
+
+![IBM.com](https://www.ibm.com/us-en?ar=1)
+
+![Wellcome to Hawai](https://www.dreamstime.com/hawaiian-green-sea-turtle-cruising-warm-waters-pacific-ocean-hawaiian-green-sea-turtle-cruising-warm-waters-image105596506)
+    
